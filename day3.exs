0264e2bca0ba1a36dfc326c280fef364dcf1c17c
tristan-orourke <tristan.orourke@gmail.com>
2024-12-03 10:34:56 -0500

    Complete day 3

--- day3.exs
+++ day3.exs
@@ -1,22 +1,49 @@
 defmodule Program do
-  @mul ~r/(?<cmd>mul)\((?<val1>\d{1,3}),(?<val2>\d{1,3})\)/
-
-  def named_captures(regex, string, options \\ []) when is_binary(string) do
-    names = Regex.names(regex)
-    options = Keyword.put(options, :capture, names)
-
-    regex
-    |> Regex.scan(string, options)
-    |> Enum.map(fn result -> Enum.zip(names, result) |> Enum.into(%{}) end)
-  end
+  @mul ~r/(mul)\((\d{1,3}),(\d{1,3})\)/
+  # @doCmd ~r/(do)\(\)/
+  # @dontCmd ~r/(don't)\(\)/
+  @programCmds ~r/(mul)\((\d{1,3}),(\d{1,3})\)|(do)\(\)|(don't)\(\)/
 
   def extract_muls(string) do
-    named_captures(@mul, string)
+    Regex.scan(@mul, string, capture: :all_but_first)
+  end
+
+  def extract_cmds(string) do
+    Regex.scan(@programCmds, string, capture: :all_but_first)
+    |> Enum.map(fn m -> Enum.filter(m, &(&1 != "")) end)
+  end
+
+  def log(_, to_return) do
+    to_return
   end
 
   def run_cmd(cmd) do
     case cmd do
-      %{"cmd" => "mul", "val1" => val1, "val2" => val2} -> String.to_integer(val1) * String.to_integer(val2)
+      ["mul", val1, val2] -> log(cmd, String.to_integer(val1) * String.to_integer(val2))
+      ["do"] -> log(cmd, 0)
+      ["don't"] -> log(cmd, 0)
+      _ -> log(cmd, 0)
+    end
+  end
+
+  def init_machine(program) do
+    %{enabled: true, program: program, output: []}
+  end
+
+  def run_program_step(machine) do
+    %{enabled: enabled, program: [cmd | program], output: output} = machine
+    case cmd do
+      ["mul", val1, val2] -> %{enabled: enabled, program: program, output: if(enabled, do: [String.to_integer(val1) * String.to_integer(val2) | output], else: output)}
+      ["do"] -> %{enabled: true, program: program, output: output}
+      ["don't"] -> %{enabled: false, program: program, output: output}
+    end
+  end
+
+  def run_program(machine) do
+    %{program: program} = machine
+    case program do
+      [] -> machine
+      _ -> run_program(run_program_step(machine))
     end
   end
 
@@ -26,7 +53,13 @@ defmodule Program do
     |> Enum.sum
   end
 
-
+  def sum_all_enabled_products(string) do
+    extract_cmds(string)
+    |> init_machine
+    |> run_program
+    |> Map.get(:output)
+    |> Enum.sum
+  end
 end
 
 exampleInput = ReadInput.read_string(("input/day3Test.txt"))
@@ -34,3 +67,6 @@ realInput = ReadInput.read_string(("input/day3.txt"))
 
 IO.inspect Program.sum_all_products(exampleInput)
 IO.inspect Program.sum_all_products(realInput)
+
+IO.inspect Program.sum_all_enabled_products(exampleInput)
+IO.inspect Program.sum_all_enabled_products(realInput)
