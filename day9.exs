4278518ee6c8869bc6c19c22a42d5948ad72a5ab
tristan-orourke <tristan.orourke@gmail.com>
2024-12-13 23:48:06 -0500

    day 9 solved

--- day9.exs
+++ day9.exs
@@ -51,6 +51,72 @@ defmodule Defragmenter do
     |> Enum.reverse()
   end
 
+  def find_first_empty_block_of_length(blocks, length, mem) do
+    last_known_position = Map.get(mem, length, 0)
+
+    if last_known_position == nil do
+      nil
+    else
+      Enum.slice(blocks, last_known_position..-1//1)
+      |> Enum.find_index(fn %{id: id, length: l} ->
+        id == :empty and l >= length
+      end)
+      |> case do
+        nil -> nil
+        index -> index + last_known_position
+      end
+    end
+  end
+
+  def compact_disk_map([], disk_map_back, _), do: disk_map_back
+
+  def compact_disk_map(disk_map_front, disk_map_back, mem) do
+    {block, disk_map_front} = List.pop_at(disk_map_front, -1)
+
+    case block.id do
+      :empty ->
+        compact_disk_map(
+          disk_map_front,
+          [block | disk_map_back],
+          mem
+        )
+
+      _ ->
+        position_of_sufficient_empty_block =
+          find_first_empty_block_of_length(disk_map_front, block.length, mem)
+
+        if position_of_sufficient_empty_block == nil do
+          compact_disk_map(
+            disk_map_front,
+            [block | disk_map_back],
+            Map.put(mem, block.length, nil)
+          )
+        else
+          empty_block =
+            Enum.at(disk_map_front, position_of_sufficient_empty_block)
+
+          space_left = empty_block.length - block.length
+
+          compact_disk_map(
+            Enum.slice(disk_map_front, 0..(position_of_sufficient_empty_block - 1)//1) ++
+              [block] ++
+              if(space_left > 0, do: [%{id: :empty, length: space_left}], else: []) ++
+              Enum.slice(disk_map_front, (position_of_sufficient_empty_block + 1)..-1//1),
+            [%{id: :empty, length: block.length} | disk_map_back],
+            Map.put(
+              mem,
+              block.length,
+              position_of_sufficient_empty_block
+            )
+          )
+        end
+    end
+  end
+
+  def compact_disk_map(disk_map) do
+    compact_disk_map(disk_map, [], Map.new())
+  end
+
   def expand_disk_map(disk_map) do
     Enum.flat_map(disk_map, fn %{id: id, length: length} -> List.duplicate(id, length) end)
   end
@@ -65,7 +131,22 @@ defmodule Defragmenter do
     |> Enum.map(fn {element, index} -> element * index end)
     |> Enum.sum()
   end
+
+  def part_2(filename) do
+    ReadInput.read_string(filename)
+    |> parse_disk_map()
+    |> compact_disk_map()
+    |> expand_disk_map()
+    # |> Enum.map(fn element -> if(element == :empty, do: ".", else: element) end)
+    # |> Enum.join()
+    |> Enum.with_index()
+    |> Enum.map(fn {element, index} -> if(element == :empty, do: 0, else: element * index) end)
+    |> Enum.sum()
+  end
 end
 
 IO.inspect(Defragmenter.part_1("input/day9Test.txt") == 1928)
-IO.inspect Defragmenter.part_1("input/day9.txt")
+IO.inspect(Defragmenter.part_1("input/day9.txt") == 6_398_252_054_886)
+
+IO.inspect(Defragmenter.part_2("input/day9Test.txt") == 2858)
+IO.inspect(Defragmenter.part_2("input/day9.txt") == 6415666220005)
